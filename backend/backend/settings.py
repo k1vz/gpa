c569c765e71f1f4595d9d02ec1669c98cca32076
k1vz <mrnerd777@gmail.com>
2024-06-30 17:40:07 -0300

    foo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['*']

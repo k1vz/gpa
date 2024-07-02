261c91c045376b3110248eb43e6b69487b0e3f94
João Gabriel de Borba Coelho <jborbacoelho@gmail.com>
2024-07-01 23:24:21 -0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,6 +66,9 @@ DATABASES = {
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 

0c0ace8a1b4d762f17a848eb6aee21ebcf3e9a87
k1vz <mrnerd777@gmail.com>
2024-07-04 00:32:57 -0300

    turning false debug mode, solving static files error

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
 from .views import HomeView
+from django.conf import settings
+from django.views.static import serve
+from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('users/', include('users.urls')),
-    path('clients/', include('clients.urls')),
-    path('tickets/', include('tickets.urls')),
-    path('drivers/', include('drivers.urls')),
-	path('', HomeView.as_view(), name='home')
+	path('', HomeView.as_view(), name='home'),
+	path('users/', include('users.urls')),
+	path('clients/', include('clients.urls')),
+	path('tickets/', include('tickets.urls')),
+	path('drivers/', include('drivers.urls')),
+
+	re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+	re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
